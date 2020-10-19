60ef26a8f550395e38a4320e77cfbf7b17b0cb24
ghostrider-05 <52837878+webbuildlucas@users.noreply.github.com>
2020-10-19 13:21:13 +0200

    Updated components
    <TotalMaps /> and <NewMaps /> are now working. <NewMaps /> gives the map names as one text block

--- docs/.vuepress/components/NewMaps.vue
+++ docs/.vuepress/components/NewMaps.vue
@@ -1,26 +1,53 @@
 <script>
 import axios from 'axios';
+const {rlappid, steam } = require('../../../data.env');
+
+let maps = [];
+let titles = []
+let LastMapscount = 5;
 
 export default {
   name: 'NewMaps',
   mounted () {
-    this.getMsg()
+    this.getMsg();
   },
   data () {
     return {
-      info: 'Initial Content'
+      info: 'Loading'
     }
   },
   methods: {
     getMsg: async function () {
-      
-    let results = getMaps(260)
+        let pages = Math.ceil(260/100);
+        
+        for(let x= 0; x<pages;x++){
+            let z = x + 1;
+            await axios
+            .get(`https://cors-anywhere.herokuapp.com/https://api.steampowered.com/IPublishedFileService/QueryFiles/v1/?key=${steam}&page=${z}&numperpage=1000&appid=${rlappid}&return_vote_data=1&return_tags=1&return_previews=1&return_children=1&return_short_description=1&return_reactions=1`)
+            .then(response => {
+                let data = response.data.response.publishedfiledetails;
+               
+                for(let x=0;x<data.length;x++){
+                    maps.push(data[x]);
+                }
+            })
+        }
+        await maps.sort((a, b) => b.time_created - a.time_created);
+     
+        let start = maps.length - LastMapscount;
+        await maps.splice(LastMapscount,start);
 
-    let promise = new Promise((resolve, reject) => {
-        setTimeout(() => resolve(results), 5000)
-    })
-    let result = await promise;
-    this.info = result;
+        for(let w=0;w<maps.length;w++){
+            titles.push(maps[w].title);
+        }
+        let results = titles.join('\n');
+    
+        let promise = new Promise((resolve, reject) => {
+            setTimeout(() => resolve(results), 5000);
+        })
+
+        let result = await promise;
+        this.info = result;
     }
   },
   render (h) {
@@ -28,35 +55,6 @@ export default {
       <div>{this.info}</div>
     )
   }
-}
-
-async function getMaps(total){
-    let maps = []
-    let pages = Math.ceil(total/100)
-    console.log(`pages:${pages}`)
-    const forLoop = async _ => {
-        console.log('start')
-        for(let x= 0; x<pages;x++){
-            let z = x + 1;
-            axios
-            .get(`https://cors-anywhere.herokuapp.com/https://api.steampowered.com/IPublishedFileService/QueryFiles/v1/?key=2EE31C799AEBCC2CE04085352B7E9A2B&page=${z}&numperpage=1000&appid=252950&return_vote_data=1&return_tags=1&return_previews=1&return_children=1&return_short_description=1&return_reactions=1`)
-            .then(response => {
-                let data = response.data.response.publishedfiledetails
-               
-                for(let x=0;x<data.length;x++){
-                    maps.push(data[x])
-                    console.log(z)
-                }
-            })
-        }
-        console.log('end')
-    }
-    await console.log(maps)
-    await maps.splice(maps.length-5,maps.length)
-    await maps.sort((a, b) => b.time_created - a.time_created);
-    await console.log(maps.length)
-    await maps.join('\n')
-   
 }
 
 </script>
--- docs/.vuepress/components/TotalMaps.vue
+++ docs/.vuepress/components/TotalMaps.vue
@@ -6,8 +6,7 @@
 
 <script>
   import axios from 'axios';
-  const  rlappid = '252950';
-  const steamAPItoken = '2EE31C799AEBCC2CE04085352B7E9A2B';
+  const {rlappid, steam } = require('../../../data.env')
 
 export default{
   data () {
@@ -17,7 +16,7 @@ export default{
   },
   beforeMount () {
     axios
-      .get(`https://cors-anywhere.herokuapp.com/https://api.steampowered.com/IPublishedFileService/QueryFiles/v1/?key=${steamAPItoken}&page=3&numperpage=1000&appid=${rlappid}&requiredtags=Maps&match_all_tags=1&required_flags=5632&child_publishedfileid=0&totalonly=1&return_details=1&strip_description_bbcode=1&return_reactions=1`)
+      .get(`https://cors-anywhere.herokuapp.com/https://api.steampowered.com/IPublishedFileService/QueryFiles/v1/?key=${steam}&page=3&numperpage=1000&appid=${rlappid}&requiredtags=Maps&match_all_tags=1&required_flags=5632&child_publishedfileid=0&totalonly=1&return_details=1&strip_description_bbcode=1&return_reactions=1`)
       .then(response => (this.info = response.data.response.total))
   }
 }
