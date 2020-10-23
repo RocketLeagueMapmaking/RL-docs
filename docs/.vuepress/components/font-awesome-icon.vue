05d00152db67c56ef073929078c567be5aba6f8d
ghostrider-05 <52837878+webbuildlucas@users.noreply.github.com>
2020-10-24 01:21:04 +0200

    fontawesome support
    Added component support for free solid font awesome icons

---
+++ docs/.vuepress/components/font-awesome-icon.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret,faDraftingCompass } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(
+    faUserSecret,
+    faDraftingCompass
+)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+export default {
+    
+}
+</script>
