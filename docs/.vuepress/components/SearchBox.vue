53dee599d11479088a05f8534d1bfb19af1835cf
ghostrider-05 <52837878+webbuildlucas@users.noreply.github.com>
2021-01-16 23:33:26 +0100

    Vuepress dev dependency

---
+++ docs/.vuepress/components/SearchBox.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="foo-layout">
+      <SearchBox/>
+  </div>
+</template>
+
+<script>
+import SearchBox from '@SearchBox'
+
+export default {
+  components: { SearchBox }
+}
+</script>
