f80a34f4f8dd5ba8d684d5021db077bf11f69fd5
spencerarrasmith <spencerarrasmith@gmail.com>
2021-11-18 20:54:09 -0700

    Adding new class, renamed styles folder

---
+++ docs/.vuepress/components/CaptionImage.vue
@@ -0,0 +1,18 @@
+<template>
+  <figure align='center' style="border: 2px solid #3E3B3B; background: #3E3B3B">
+    <img :src="imagesrc" :width="width" alt=""/>
+    <hr>
+    <figcaption color: white; margin-top: -0.1rem><i><small>{{ caption }}</small></i></figcaption>
+  </figure>
+</template>
+
+<script>
+export default {
+  props: ['src', 'caption', 'width'],
+  computed: {
+    imagesrc () {
+      return  this.src
+    }
+  }
+}
+</script>
