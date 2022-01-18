f6cbd725e9fe6ce28e17e60a96bd4366d4175f55
ghostrider-05 <52837878+ghostrider-05@users.noreply.github.com>
2022-01-18 19:10:20 +0100

    update actions
    - add deploy to netlify
    - add link checking
    - add deploy script with ssh

--- docs/.vuepress/configs/plugins.js
+++ docs/.vuepress/configs/plugins.js
@@ -1,5 +1,34 @@
-module.exports = [
+const process = require('process')
+const additionalPages = require('./pages.js')
+
+/**
+ * Filter empty plugins
+ * @param {*[]} plugins 
+ */
+const definePlugins = (plugins) => {
+    return plugins.filter(plugin => plugin != undefined && (plugin.length || 1) > 0)
+}
+
+module.exports = definePlugins([
     '@vuepress/active-header-links',
     '@vuepress/last-updated',
-    'seo'
-]
+    'seo',
+    process.env.npm_config_test === 'links' ? [
+        // Only runs when the test script in package.json is run
+        require('../plugins/linkCheck.js'),
+        {
+            // Any additional pages
+            additionalPages,
+            // Log valid links
+            logMatches: false,
+            // Set to false to always exit with a 0 code
+            exitOnErrors: true,
+            // Link check configuration
+            links: {
+                internal: true,
+                internalSlugs: true,
+                external: false
+            }
+        }
+    ] : []
+])
