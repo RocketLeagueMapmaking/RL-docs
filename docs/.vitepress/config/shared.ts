55b0b4a7e241c496c441abfdb5ca2267108b0bfc
ghostrider-05 <lucas.duijn@hotmail.nl>
2024-08-08 17:15:09 +0200

    Use constants in configuration

---
+++ docs/.vitepress/config/shared.ts
@@ -0,0 +1,7 @@
+export const DISCORD_INVITE = 'https://discord.gg/PWu3ZWa'
+
+export const GITHUB_REPOSITORY = 'rocketleaguemapmaking/rl-docs'
+
+export const GITHUB_REPOSITORY_URL = `https://github.com/${GITHUB_REPOSITORY}`
+
+export const GITHUB_DEFAULT_BRANCH = 'master'
