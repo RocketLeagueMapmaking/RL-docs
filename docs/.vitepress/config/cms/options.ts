26794937852730b70c7c730c75cda43affe8c9c0
ghostrider-05 <lucas.duijn@hotmail.nl>
2024-08-08 17:15:22 +0200

    Add configuration for CMS

---
+++ docs/.vitepress/config/cms/options.ts
@@ -0,0 +1,31 @@
+import { createField, type VitePress } from "vite-plugin-decap-cms";
+import { SidebarItemConfig } from "../sidebar";
+
+export const mediaFolder = 'docs/.vitepress/public/images/'
+
+export function createFolderOptions(label: string, config: SidebarItemConfig) {
+    return {
+        overwrites: {
+            titleTemplate: { hidden: true },
+            head: { hidden: true },
+        },
+        collection: {
+            label,
+            labelSingular: label + ' file',
+            description: config.description,
+            mediaFolder: mediaFolder + config.mediaFolder,
+            create: true,
+        },
+        markdownOptions: {
+            modes: ['raw', 'rich_text'],
+            // buttons: [],
+            // editorComponents: [],
+        },
+        additionalFields: [
+            createField('string', {
+                name: 'filename',
+                label: 'File name',
+            }),
+        ]
+    } satisfies Parameters<typeof VitePress['createDefaultPageFolderCollection']>[2]
+}
