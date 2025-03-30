b7372259b8f3375ae0eeeae8431340ae583f3c13
Spencer Arrasmith <spencerarrasmith@gmail.com>
2025-03-30 12:57:33 -0600

    Update move_dummyassets.sh

--- deployment/move_dummyassets.sh
+++ deployment/move_dummyassets.sh
@@ -3,5 +3,5 @@
 HASH=$(ls -lat | sed '4!d' | rev | cut -d' ' -f 1 | rev)
 HASH=$(docker inspect rlmm | grep MergedDir | rev | cut -d' ' -f 1 | rev | cut -d'"' -f2 | cut -d'"' -f1)
 
-chmod 644 /home/rocketleaguemapmaking/RL_NotSoDummyAssets-main.zip
+# chmod 644 /home/rocketleaguemapmaking/RL_NotSoDummyAssets-main.zip
 cp /home/rocketleaguemapmaking/RL_NotSoDummyAssets-main.zip ${HASH}/usr/share/nginx/html/resources/
